74e9b1f9ab3a80f91fd88b876d4e030f46e40f6d
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-26 19:10:40 -0700

    create examples of strict variables

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,18 @@
 "use strict";
 // need to assign a type to the variable
 let id = 5;
+let company = "Belltron 3030";
+let isPublished = true;
+// any type can be any type of value 
+let x = "hello";
+x = true;
+let age;
+age = 20;
+let ids = [1, 2, 3, 4, 5];
+// will not work
+ids.push("Hello");
+// Will work
+let arr = [1, true, "hello"];
 console.log("ID", id);
 // will not reassign a variable because it is of a different type
 // id = '5';
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 // need to assign a type to the variable
 let id: number = 5;
+let company: string = "Belltron 3030";
+let isPublished: boolean = true;
+// any type can be any type of value 
+let x : any = "hello";
+
+x = true;
+
+let age: number
+age = 20;
+
+let ids: number[]= [1,2,3,4,5];
+// will not work
+ids.push("Hello")
+// Will work
+let arr: any[] = [1,true, "hello"];
 
 console.log("ID", id);
 
