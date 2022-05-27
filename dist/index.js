5f644a8108521c1a5a2f418c49d6386107a7bc8b
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-26 19:13:58 -0700

    tuple example

--- dist/index.js
+++ dist/index.js
@@ -10,9 +10,17 @@ let age;
 age = 20;
 let ids = [1, 2, 3, 4, 5];
 // will not work
-ids.push("Hello");
+// ids.push("Hello")
 // Will work
 let arr = [1, true, "hello"];
-console.log("ID", id);
+// Tuple
+let person = [1, "Chris", false];
+// Tuple array
+let employee;
+employee = [
+    [1, "Chris"],
+    [2, "Brad"],
+    [1, "John"],
+];
 // will not reassign a variable because it is of a different type
 // id = '5';
--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,21 @@ age = 20;
 
 let ids: number[]= [1,2,3,4,5];
 // will not work
-ids.push("Hello")
+// ids.push("Hello")
+
 // Will work
 let arr: any[] = [1,true, "hello"];
 
-console.log("ID", id);
+// Tuple
+let person: [number, string, boolean] = [1, "Chris", false];
+
+// Tuple array
+let employee: [number, string][]
+employee = [
+  [1, "Chris"],
+  [2, "Brad"],
+  [1, "John"],
+]
 
 // will not reassign a variable because it is of a different type
 // id = '5';
